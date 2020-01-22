62a92a9bea2eea0e61c0e250f51779b04a547126
Martin Chaplin <martin.chaplin44@gmail.com>
2020-01-22 03:12:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,16 @@
 class Dog
   
-  attr_accessor :name
-   @@all = []
-  def intialize
-     
-    @name = name
-    @@all<< self
-    end
+class Dog
 
-    def self.clear_all
+	@@all = []
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def self.clear_all
 		@@all.clear
 
 	end
-end
